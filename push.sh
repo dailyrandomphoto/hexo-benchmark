0a7c0ba2f52f8931cc21ac835693501244041855
dailyrandomphoto <dailyrandomphoto@gmail.com>
2020-01-08 01:33:07 +0800

    Initial commit

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+setup_git() {
+  git config user.email "travis@travis-ci.org"
+  git config user.name "Travis CI"
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/dailyrandomphoto/hexo-benchmark-data.git > /dev/null 2>&1
+}
+
+commit_files() {
+  git add --all
+  git commit --message "Benchmark on Travis: #$TRAVIS_BUILD_NUMBER for $TRAVIS_BRANCH on node-$TRAVIS_NODE_VERSION"
+}
+
+upload_files() {
+  git pull -r -f origin master
+  git push --quiet --set-upstream origin-pages master
+  if [ $? -gt 0 ]; then
+    echo "push again"
+    upload_files
+  fi
+}
+
+setup_git
+commit_files
+upload_files
